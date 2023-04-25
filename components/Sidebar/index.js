721a6ac03391e28647b90926519a76f5dbd39cd8
Bobby Hall Jr <bobby@Bobbys-MacBook-Pro.local>
2023-04-25 18:19:00 -0400

    finished profile page UI 2nd attempt

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -3,7 +3,7 @@ import React, { useRef } from "react";
 import classNames from "classnames";
 import Link from "next/link";
 import Image from "next/image";
-import { defaultNavItems } from "../DefaultNavItems";
+import { defaultNavItems } from "../defaultNavItems";
 import Avatar from "../Avatar";
 
 const Sidebar = ({ open, navItems = defaultNavItems }) => {
